9c354f11b8d01937cf4ef507ed0f5cc104a446d5
Alexander Sverchkov <amsverchkov@mail.ru>
2023-10-09 17:09:02 +0300

    Поправлена ошибка в дто

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,3 +1,28 @@
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString, IsStrongPassword } from 'class-validator';
 
-export class UpdateUserDto extends CreateUserDto {}
+export class UpdateUserDto {
+	@IsOptional()
+	@IsEmail()
+	@ApiProperty()
+	email?: string;
+
+	@IsOptional()
+	@IsString()
+	@ApiProperty()
+	name?: string;
+
+	@IsOptional()
+	@IsString()
+	@ApiProperty()
+	surname?: string;
+
+	@IsOptional()
+	@IsStrongPassword(null, {
+		message:
+			'Email must contain at least 8 characters consisting of capital letters' +
+			'and lowercase Latin letters as well as specials characters',
+	})
+	@ApiProperty()
+	password?: string;
+}
